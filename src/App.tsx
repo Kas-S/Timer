daac7ef6e2a68ccb1e94c83f742879fe9692666a
Kas S <sotimkul@gmail.com>
2024-07-10 21:00:38 +0500

    Start and stop functions implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import {Timer, Buttons} from "./components"
-import {useState, useCallback, useRef} from "react"
+import {useState, useCallback, useRef, useEffect} from "react"
 
 function App() {
   const [minutes, setMinutes] = useState(3),
-        [seconds, setSeconds] = useState(0)
+        [seconds, setSeconds] = useState(0),
+        [started, setStarted] = useState(false)
   const initialMinutes = useRef(3),
         initialSeconds = useRef(0)
+
+    useEffect(() => {
+        if (started) {
+            let m = minutes,
+                s = seconds
+            setInterval(() => {
+                if (s - 1 === -1 && m > 0) {
+                    s = 59
+                    m--
+                } else {
+                    s--
+                }
+                setMinutes(m)
+                setSeconds(s)
+                if (s === 0 && m === 0) {
+                    stop()
+                }
+            }, 1000)
+        }
+    }, [started]);
 
   const updateMinutes = (v: number) => {
       setMinutes(v)
@@ -17,13 +38,18 @@ function App() {
       initialSeconds.current = v
   }
 
-  const start = useCallback(() => {}, [])
+  const start = useCallback(() => {
+      setStarted(true)
+
+  }, [])
 
   const reset = useCallback(() => {}, [])
 
   const pause = useCallback(() => {}, [])
 
-  const stop = useCallback(() => {}, [])
+  const stop = useCallback(() => {
+        setStarted(false)
+  }, [])
 
   return (
     <>
