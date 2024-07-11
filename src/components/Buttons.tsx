2c8b08dd2b619d1c508904acc84feedfd40562b0
Kas S <sotimkul@gmail.com>
2024-07-11 15:19:15 +0500

    Pause button deleted

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,22 +3,20 @@ import {memo} from "react"
 interface ButtonsProps {
     start: () => void,
     stop: () => void,
-    reset: () => void,
-    pause: () => void
+    reset: () => void
 }
 
 const Button = ({content, func}: {content: string, func: () => void}) => (
     <button type="button" onClick={func} className="p-4 rounded bg-blue-400 text-white font-bold text-lg">{content}</button>
 )
 
-function Buttons ({start, stop, reset, pause}: ButtonsProps) {
+function Buttons ({start, stop, reset}: ButtonsProps) {
     console.log("Buttons rendered")
     return (
         <div className="flex items-center justify-center w-full mt-10 gap-3">
             <Button func={start} content="Start"/>
             <Button func={stop} content="Stop"/>
             <Button func={reset} content="Reset"/>
-            <Button func={pause} content="Pause"/>
         </div>
     )
 }
